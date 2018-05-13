8bbecdbd7dd7d3bcd200a48d2c28ef935b14f4e1
Per Lundholm <per@artcomputer.se>
2018-05-13 12:32:54 +0200

    Example of temporary showing something

---
+++ src/delay.elm
@@ -0,0 +1,39 @@
+module Main exposing (main)
+
+import Html exposing (Html, text, div, button)
+import Html.Events exposing (onClick)
+import Task
+import Process
+import Time
+type alias Model =
+    { message : String }
+
+
+init =
+    ( { message = "nothing yet" }, Cmd.none )
+
+
+main = Html.program
+    { view = view
+    , init = init
+    , update = update
+    , subscriptions = \_ -> Sub.none
+    }
+
+
+view model =
+    div [] [ button [ onClick Login ] [ text "click me!" ]  , text model.message ]
+
+type Msg = Login | ShowDashboard Time.Time
+
+update msg model =
+    case msg of
+        Login ->
+            ( { model | message = "temporary message" }
+            , Task.perform ShowDashboard (Process.sleep 2000 |> Task.andThen (\_ -> Time.now))
+            )
+
+        ShowDashboard _ ->
+            ( { model | message = "" }
+            , Cmd.none
+            )
