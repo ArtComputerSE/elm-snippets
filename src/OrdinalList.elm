98ffb16776f79a6766582dba8f95ab00716aa825
Per Lundholm <per@artcomputer.se>
2019-02-15 11:11:55 +0100

    Added move right function.

--- src/OrdinalList.elm
+++ src/OrdinalList.elm
@@ -1,4 +1,4 @@
-module OrdinalList exposing (moveLeft)
+module OrdinalList exposing (moveLeft, moveRight)
 
 
 moveLeft : List { a | ordinal : Int } -> Int -> List { a | ordinal : Int }
@@ -16,3 +16,20 @@ moveLeft list ordinal =
 
             else
                 a :: moveLeft (b :: tail) ordinal
+
+
+moveRight : List { a | ordinal : Int } -> Int -> List { a | ordinal : Int }
+moveRight list ordinal =
+    case list of
+        [] ->
+            []
+
+        [ single ] ->
+            [ single ]
+
+        a :: b :: tail ->
+            if a.ordinal == ordinal then
+                { b | ordinal = a.ordinal } :: { a | ordinal = b.ordinal } :: tail
+
+            else
+                a :: moveRight (b :: tail) ordinal
--- tests/OrdinalListTest.elm
+++ tests/OrdinalListTest.elm
@@ -1,7 +1,7 @@
 module OrdinalListTest exposing (suite)
 
 import Expect exposing (equal)
-import OrdinalList exposing (moveLeft)
+import OrdinalList exposing (moveLeft, moveRight)
 import Test exposing (describe, test)
 
 
@@ -28,17 +28,35 @@ elem3 =
 
 suite : Test.Test
 suite =
-    describe "move left suite"
-        [ test "move left of an empty list is empty" <|
-            \() -> equal [] (moveLeft [] 9)
-        , test "move left of list of one element changes nothing " <|
-            \() -> equal [ TestType "x" 0 ] (moveLeft [ TestType "x" 0 ] 42)
-        , test "move left of two list element should swap them." <|
-            \() -> equal [ TestType elem2.payLoad 0, TestType elem1.payLoad 1 ] (moveLeft [ elem1, elem2 ] 1)
-        , test "move left of three list element should swap them." <|
-            \() ->
-                equal [ TestType elem1.payLoad 0, TestType elem3.payLoad 1, TestType elem2.payLoad 2 ]
-                    (moveLeft [ elem1, elem2, elem3 ] 2)
-        , test "move left beyond list length changes nothing " <|
-            \() -> equal [ elem1, elem2 ] (moveLeft [ elem1, elem2 ] 3)
+    describe "Ordinal list test suite."
+        [ describe "move left suite"
+            [ test "move left of an empty list is empty" <|
+                \() -> equal [] (moveLeft [] 9)
+            , test "move left of list of one element changes nothing " <|
+                \() -> equal [ TestType "x" 0 ] (moveLeft [ TestType "x" 0 ] 42)
+            , test "move left of two list element should swap them." <|
+                \() -> equal [ TestType elem2.payLoad 0, TestType elem1.payLoad 1 ] (moveLeft [ elem1, elem2 ] 1)
+            , test "move left of three list element should swap them." <|
+                \() ->
+                    equal [ TestType elem1.payLoad 0, TestType elem3.payLoad 1, TestType elem2.payLoad 2 ]
+                        (moveLeft [ elem1, elem2, elem3 ] 2)
+            , test "move left beyond list length changes nothing " <|
+                \() -> equal [ elem1, elem2 ] (moveLeft [ elem1, elem2 ] 3)
+            ]
+        , describe "move right suite"
+            [ test "move right of an empty list is empty" <|
+                \() -> equal [] (moveRight [] 9)
+            , test "move right of list of one element changes nothing " <|
+                \() -> equal [ TestType "x" 0 ] (moveRight [ TestType "x" 0 ] 42)
+            , test "move right of two list element should swap them." <|
+                \() ->
+                    equal [ TestType elem2.payLoad 0, TestType elem1.payLoad 1 ]
+                        (moveRight [ elem1, elem2 ] 0)
+            , test "move right of three list element should swap them." <|
+                \() ->
+                    equal [ TestType elem1.payLoad 0, TestType elem3.payLoad 1, TestType elem2.payLoad 2 ]
+                        (moveRight [ elem1, elem2, elem3 ] 1)
+            , test "move right beyond list length changes nothing " <|
+                \() -> equal [ elem1, elem2 ] (moveRight [ elem1, elem2 ] 3)
+            ]
         ]
