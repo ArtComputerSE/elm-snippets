cd1ed1381e9d646e0beaf38cb5e238fbea2a100a
Per Lundholm <per@artcomputer.se>
2019-11-01 10:06:07 +0100

    Documented Matrix as if it was a package.

--- src/Matrix.elm
+++ src/Matrix.elm
@@ -1,23 +1,57 @@
-module Matrix exposing (Matrix, empty, get, getXs, getYs, initialize, set, size)
+module Matrix exposing
+    ( Matrix
+    , empty, initialize
+    , size, get, getXs, getYs
+    , set
+    )
+
+{-| Two dimensional matrix backed by Array from the Elm core, the fast immutable array
+implementation.
+
+
+# Matrix
+
+@docs Matrix
+
+
+# Creation
+
+@docs empty, initialize
+
+
+# Query
+
+@docs size, get, getXs, getYs
+
+
+# Manipulate
+
+@docs set
+
+-}
 
 import Array exposing (Array)
 
 
-
-{-
-   Implement a two dimensional matrix using arrays.
+{-| Representation of immutable, two dimensional matrix. You can create a matrix of integers
+(`Matrix Int`) or strings (`Matrix String`) or any other type of value you can dream up.
 -}
-
-
 type alias Matrix a =
     Array (Array a)
 
 
+{-| Return an empty matrix.
+
+    size empty == ( 0, 0 )
+
+-}
 empty : Matrix a
 empty =
     Array.empty
 
 
+{-| Return the size of a matrix in the form of a tuple, (sizeX, sizeY).
+-}
 size : Matrix a -> ( Int, Int )
 size matrix =
     let
@@ -35,11 +69,16 @@ size matrix =
     ( sizeX, sizeY )
 
 
+{-| Initialize a matrix, given desired size and a function for the value of a cell,
+given its x and y.
+-}
 initialize : Int -> Int -> (Int -> Int -> a) -> Matrix a
 initialize sizeX sizeY fn =
     Array.initialize sizeX (\col -> Array.initialize sizeY (fn col))
 
 
+{-| Set the cell at (x,y) to a new value. If the (x,y) is out of bounds, silently do nothing,
+-}
 set : Matrix a -> Int -> Int -> a -> Matrix a
 set matrix x y v =
     case Array.get x matrix of
@@ -50,16 +89,22 @@ set matrix x y v =
             matrix
 
 
+{-| Maybe get the value of the cell at (x,y).
+-}
 get : Matrix a -> Int -> Int -> Maybe a
 get matrix x y =
     getXs matrix x |> Array.get y
 
 
+{-| Get all values along a given x as an array. If x is out of bounds, return an empty array.
+-}
 getXs : Matrix a -> Int -> Array a
 getXs matrix x =
     Maybe.withDefault Array.empty (Array.get x matrix)
 
 
+{-| Get all values along a given y as an array. If y is out of bounds, return an empty array.
+-}
 getYs : Matrix a -> Int -> Array a
 getYs matrix y =
     Array.toList matrix |> pickY y |> Array.fromList
