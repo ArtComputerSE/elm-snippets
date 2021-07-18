77df2aa74b02fc617428bd4bac9f2aac136cda7e
Per Lundholm <per@artcomputer.se>
2021-07-18 14:47:38 +0200

    Move the image as we move the mouse.

--- src/CropImage/ElmUIExample.elm
+++ src/CropImage/ElmUIExample.elm
@@ -1,64 +1,84 @@
 module CropImage.ElmUIExample exposing (Model, Msg(..), main, update, view)
 
 import Browser
-import Element exposing (behindContent, clip, column, height, image, moveDown, moveRight, px, width)
+import Element exposing (behindContent, centerX, clip, column, fill, height, image, moveDown, moveRight, padding, px, width)
 import Element.Border as Border
 import Html
 import Html.Events.Extra.Mouse
 
 
+type DragState
+    = Initial
+    | Dragging ( Float, Float )
+
+
 type alias Model =
-    { dragStart : ( Float, Float )
+    { dragState : DragState
     , offset : ( Float, Float )
     }
 
 
 type Msg
-    = MouseDown
-    | DownMsg ( Float, Float )
+    = DownMsg ( Float, Float )
+    | MoveMsg ( Float, Float )
     | UpMsg ( Float, Float )
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { dragStart = ( 0, 0 ), offset = ( 0, 0 ) }, Cmd.none )
+    ( { dragState = Initial, offset = ( 0, 0 ) }, Cmd.none )
 
 
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
-        MouseDown ->
-            let
-                _ =
-                    Debug.log "mouse down"
-            in
-            ( model, Cmd.none )
-
         DownMsg ( x, y ) ->
             let
                 _ =
                     Debug.log "mouse down" ( x, y )
             in
-            ( { model | dragStart = ( x, y ) }, Cmd.none )
+            ( { model | dragState = Dragging ( x, y ) }, Cmd.none )
+
+        MoveMsg ( x, y ) ->
+            case model.dragState of
+                Initial ->
+                    ( model, Cmd.none )
+
+                Dragging ( sx, sy ) ->
+                    let
+                        _ =
+                            Debug.log "mouse move" ( dx, dy )
+
+                        ( dx, dy ) =
+                            ( x - sx, y - sy )
+
+                        ( ox, oy ) =
+                            model.offset
+                    in
+                    ( { model | dragState = Dragging ( x, y ), offset = ( ox + dx, oy + dy ) }, Cmd.none )
 
         UpMsg ( x, y ) ->
-            let
-                _ =
-                    Debug.log "mouse up" ( x, y )
+            case model.dragState of
+                Initial ->
+                    ( model, Cmd.none )
 
-                _ =
-                    Debug.log "moved" ( x - dx, y - dy )
+                Dragging ( sx, sy ) ->
+                    let
+                        _ =
+                            Debug.log "mouse up" ( x, y )
 
-                ( sx, sy ) =
-                    model.dragStart
+                        ( dx, dy ) =
+                            ( x - sx, y - sy )
 
-                ( dx, dy ) =
-                    ( x - sx, y - sy )
-
-                ( ox, oy ) =
-                    model.offset
-            in
-            ( { model | offset = ( ox + dx, oy + dy ) }, Cmd.none )
+                        ( ox, oy ) =
+                            model.offset
+                    in
+                    ( { model
+                        | dragState = Initial
+                        , offset = ( Basics.min 0.0 (ox + dx), Basics.min 0.0 (oy + dy) )
+                      }
+                    , Cmd.none
+                    )
 
 
 photoUrl =
@@ -75,16 +95,21 @@ view model =
             image [ moveDown oy, moveRight ox ] { src = photoUrl, description = "cute" }
     in
     Element.layout [] <|
-        column
-            [ Border.width 1
-            , width (px 300)
-            , height (px 300)
-            , behindContent theImage
-            , Element.htmlAttribute <| Html.Events.Extra.Mouse.onDown (\event -> DownMsg event.clientPos)
-            , Element.htmlAttribute <| Html.Events.Extra.Mouse.onUp (\event -> UpMsg event.clientPos)
-            , clip
+        column [ width fill, padding 10 ]
+            [ column
+                [ Border.width 1
+                , width (px 300)
+                , height (px 300)
+                , behindContent theImage
+                , Element.htmlAttribute <| Html.Events.Extra.Mouse.onDown (\event -> DownMsg event.clientPos)
+                , Element.htmlAttribute <| Html.Events.Extra.Mouse.onMove (\event -> MoveMsg event.clientPos)
+                , Element.htmlAttribute <| Html.Events.Extra.Mouse.onUp (\event -> UpMsg event.clientPos)
+                , Element.htmlAttribute <| Html.Events.Extra.Mouse.onLeave (\event -> UpMsg event.clientPos)
+                , clip
+                , centerX
+                ]
+                []
             ]
-            []
 
 
 main : Program () Model Msg
---
+++ src/CropImage/ElmUIExample.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+(cd ../.. ; elm-live src/CropImage/ElmUiExample.elm --port 1337 -- --debug)
